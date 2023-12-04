82a2f73b3f55373f1aeb95f40e90199ed840c819
TR <2993078435@qq.com>
2023-12-04 17:39:13 +0800

    更改了 deploy.sh和vue.config.js 的配置

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@gitee.com:T1R/Manei.git master:gh-pages
+git push -f git@github.com:gla1veNG/Manei.git master:gh-pages
 
 cd -
