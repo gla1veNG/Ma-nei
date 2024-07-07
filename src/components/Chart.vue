6e03c6296591ef2a0442e48595e595b8c7f97908
Ji7mmy <2993078435@qq.com>
2024-07-07 16:29:35 +0800

    通过监听 options 的变化改变 chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -3,26 +3,32 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import * as echarts from "echarts";
 import { EChartsOption } from "echarts";
 
 @Component
 export default class Chart extends Vue {   
   @Prop() options?: EChartsOption;
+  chart?:echarts.ECharts;
 
   mounted() {
     if(this.options === undefined){
        return console.error('options 为空');
     }
-    const chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
-    chart.setOption(this.options);
+    this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+    this.chart.setOption(this.options);
   }
-}
+
+  @Watch('options')
+    onOptionsChange(newValue:EChartsOption){
+      this.chart!.setOption(newValue);
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-.wrapper{
-    height: 400px;
+.wrapper {
+  height: 400px;
 }
 </style>
