f52f2969bc0c772098107ee83de0887e92064ee1
Ji7mmy <2993078435@qq.com>
2024-07-07 20:30:42 +0800

    使用 webStorm

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <router-view/>  
-</div>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
@@ -9,18 +9,18 @@
 @import "~@/assets/style/helper.scss";
 @import "~@/assets/style/reset.scss";
 
-  body{
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #333;
-    font-family: $font-hei;
-    line-height: 1.5;
-    font-size: 16px;
-    background: #f5f5f5;
-  }
-  
-  #app{
-    max-width: 400px;
-    margin: 0 auto;
-  }
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  font-family: $font-hei;
+  line-height: 1.5;
+  font-size: 16px;
+  background: #f5f5f5;
+}
+
+#app {
+  max-width: 400px;
+  margin: 0 auto;
+}
 </style>
